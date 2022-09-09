af65b50a218348333971c1369a42d3647f8b40d2
jocodvc <jcorbijn@davinci.nl>
2022-09-09 11:55:19 +0200

    added spiekbriefje

---
+++ spiekbriefje.py
@@ -0,0 +1,34 @@
+# commentaar
+# start met een hekje
+########################################################################
+# datatypes
+# string = woord of zin
+########################################################################
+woord = 'hallo'  # het mag ook met dubbele komma's
+woord = "hallo"
+# met drie quotes krijg je een multiline string
+lange_zin = '''dit is een erg lange zin,
+die verdeeld is over meerdere regels.'''
+
+# int = geheel getal
+x = 6
+# float = kommagetal (let op punt ipv komma
+y = 6.5
+
+##########################################################################
+# built in functies bif's (ongeveer 70 in Python)
+##########################################################################
+# input = stel een vraag en sla het antwoord op. Geeft altijd een string terug.
+antwoord = input('Hoe heet je?')
+
+# print = stuurt een string naar de commandline
+print('hallo', antwoord)  # of
+print('hallo ' + antwoord)  # of
+print(f'hallo {antwoord}, leuk kennis te maken!')
+
+
+# int() = zet om naar naar int
+getal = int('6')
+
+
+
